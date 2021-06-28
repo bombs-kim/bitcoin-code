c4d78f613c3a837e9fe15b161805a73e6d493242
Beomsoo Kim <bluewhale8202@gmail.com>
2021-06-28 19:42:18 +0900

    Restored make_transaction

---
+++ make_transaction.py
@@ -0,0 +1,18 @@
+import key_utils
+import txn_utils
+
+
+privateKey = key_utils.wif_to_private_key(
+    "5HusYj2b2x4nroApgfvaSfKYZhRbKFH41bVyPooymbC6KfgSXdD") #1MMMM
+
+signed_txn = txn_utils.make_signed_transaction(
+    privateKey,
+    "81b4c832d70cb56ff957589752eb4125a4cab78a25a8fc52d6a09e5bd4404d48", # output (prev) transaction hash
+    0, # sourceIndex
+    key_utils.addr_hash_to_script_pub_key("1MMMMSUb1piy2ufrSguNUdFmAcvqrQF8M5"),
+    [
+        [91234, #satoshis
+        key_utils.addr_hash_to_script_pub_key("1KKKK6N21XKo48zWKuQKXdvSsCf95ibHFa")]])
+
+txn_utils.verify_txn_signature(signed_txn)
+print('SIGNED TXN', signed_txn)
